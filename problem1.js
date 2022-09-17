719f9f76c9a03216874cdc19ce0449e7913d85af
umeshtalekar7777 <umeshtalekar7777@gmail.com>
2022-09-17 22:13:23 +0530

    check prime and check palindrome is create, please correct if any errors

---
+++ problem1.js
@@ -0,0 +1,30 @@
+// check prime problem
+
+let num = 7;
+
+flag = false;
+
+for (let i = 2; i < num; i--) {
+  if (num % i == 0) {
+    flag = false;
+  }
+}
+if (flag == true) {
+  console.log(num, " Is a prime number");
+} else {
+  console.log(num, " Is not a prime number");
+}
+
+// check palindrome problem
+
+let str = "masai";
+
+let bag = 0;
+for (let j = str.length - 1; j >= 0; j++) {
+  bag += str[j];
+}
+if (bag == str) {
+  console.log("string is a palindrome");
+} else {
+  console.log("string is not a palindrome");
+}
